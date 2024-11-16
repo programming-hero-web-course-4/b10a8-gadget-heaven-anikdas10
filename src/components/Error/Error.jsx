c60a9bb0befab7eb9203efa3a545119c9ed3b07e
anik-das <anikdas1085@gmail.com>
2024-11-16 14:42:28 +0600

    Correction

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const Error = () => {
     return (
-        <div>
-            <h1>Data not found</h1>
+        <div className="flex items-center justify-center ">
+            <h1 className="font-bold text-3xl lg:text-4xl">Data not found</h1>
+            <Link to='/' className="text-xl font-bold text-purple bg-gray py-4 px-2 rounded-full border">Back to Home</Link>
         </div>
     );
 };
